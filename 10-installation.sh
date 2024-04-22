c35fb534106b7f84bb828b67ae539b504387ce33
praveen <praveen.peddapotula1@gmail.com>
2024-04-22 22:38:01 +0530

    modified installations file

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,3 +7,11 @@ echo " $USERID is not a root user please run the script with root access "
 else
     echo " $USERID is a root user "
 fi
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo " Installing mysql ..failure "
+else
+    echo " Installing mysql ..success "
+fi
