526dbe10169c008fce5c86d460cc7d7bd89c0474
praveen <praveen.peddapotula1@gmail.com>
2024-04-22 19:47:24 +0530

    updated variables file

--- 01-variables.sh
+++ 01-variables.sh
@@ -2,5 +2,5 @@
 person1=$1
 person2=$2
 
-$person1 : "Hi $person2 how r u"
-$person2 : "Hello $person1 im fine how abt u"
+echo " $person1 : Hi $person2 how r u"
+echo "$person2 : Hello $person1 im fine how abt u"
