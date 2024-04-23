12319cc538a5800a87dcc244673943c13b881818
praveen <praveen.peddapotula1@gmail.com>
2024-04-23 11:46:15 +0530

    added functions file

---
+++ 11-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=(date +%F-%H-%M-%S)
+SCRIPT_NAME=( $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo " $2 ..FAILURE "
+    else
+    echo" $1 .. SUCCESS "
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run the script with root access "
+else
+    echo "you are root user "
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing mysql"
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing git "
