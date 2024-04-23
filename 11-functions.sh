872e5e2223c1cb7e2a5ce37ed90370eb6b77908e
praveen <praveen.peddapotula1@gmail.com>
2024-04-23 15:58:27 +0530

    5th modification-functions file

--- 11-functions.sh
+++ 11-functions.sh
@@ -7,10 +7,10 @@ LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-    echo "$2 ..FAILURE"
+        echo "$2 ..FAILURE"
     exit 1
     else
-    echo"$2 .. SUCCESS"
+        echo "$2 .. SUCCESS"
     fi
 }
 
