3092bc0edcc40c6b490571e51caa0168ab61cc59
praveen <praveen.peddapotula1@gmail.com>
2024-04-23 21:49:37 +0530

    modified installation file

--- 15-installationpackages.sh
+++ 15-installationpackages.sh
@@ -9,10 +9,11 @@ N="/e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-    echo "$2..$R FAILURE $N"
+    echo -e "$2..$R FAILURE $N"
     else
-    echo "$2..$G SUCCESS $N" 
-}
+    echo -e "$2..$G SUCCESS $N" 
+    fi
+    }
 
 if [ $USERID -ne 0 ]
 then
